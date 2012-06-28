af12d8cfc48ccbd2d429e00a111478a791be3205
Matt Riley <mriley@gmail.com>
2012-06-28 00:48:14 -0700

    bumping version again

--- swiftype.php
+++ swiftype.php
@@ -5,7 +5,7 @@ Plugin Name: Swiftype Search
 Plugin URI: http://swiftype.com
 Description: The Swiftype Search plugin replaces the standard WordPress search with a better search engine that is fully customizable via the Swiftype dashboard. The Swiftype dashboard lets you customize the results for any search keyword via a drag-and-drop interface.
 Author: Swiftype, Inc.
-Version: 1.0.3
+Version: 1.0.4
 Author URI: http://swiftype.com
 */
 
