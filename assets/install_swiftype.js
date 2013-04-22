9fa7bfabe28846a08059b9fcfc7835f929cfbd48
Quin Hoxie <qhoxie@gmail.com>
2013-04-22 14:52:44 -0700

    add configuration hooks for autocomplete javascript

--- assets/install_swiftype.js
+++ assets/install_swiftype.js
@@ -9893,25 +9893,44 @@ window['$stjq'] = jQuery.noConflict(true);
 			Swiftype.engineKey = swiftypeParams.engineKey;
 			Swiftype.inputElements = $('input[name=s]');
 
+			function readSwiftypeConfigFor(option) {
+			  if ((typeof window.swiftypeConfig === 'undefined') || (typeof window.swiftypeConfig[option] === 'undefined') || window.swiftypeConfig[option] === null) {
+			    return undefined;
+			  }
+
+			  return window.swiftypeConfig[option];
+			}
+
+			var SwiftypeConfigManager = {
+			  getFilters: function() {
+			    return readSwiftypeConfigFor('filters');
+			  },
+			  getSearchFields: function() {
+			    return readSwiftypeConfigFor('searchFields');
+			  },
+			  getSortField: function() {
+			    return readSwiftypeConfigFor('sortField');
+			  },
+			  getSortDirection: function() {
+			    return readSwiftypeConfigFor('sortDirection');
+			  }
+			};
+
+			var swiftypeOptions = {
+			  onComplete: function(dataItem, prefix) {
+			    Swiftype.pingAutoSelection(Swiftype.engineKey, dataItem['id'], prefix, function() { window.location = dataItem['url']; });
+			  },
+			  documentTypes: ['posts'],
+			  engineKey: Swiftype.engineKey,
+			  filters: SwiftypeConfigManager.getFilters(),
+			  searchFields: SwiftypeConfigManager.getSearchFields(),
+			  sortField: SwiftypeConfigManager.getSortField(),
+			  sortDirection: SwiftypeConfigManager.getSortDirection()
+			};
+
 			$.each(Swiftype.inputElements, function(idx, el) {
 				var $el = $(el);
-				$el.swiftype({
-					onComplete: function(dataItem,prefix) {
-						Swiftype.pingAutoSelection(Swiftype.engineKey, dataItem['id'], prefix, function() { window.location = dataItem['url']; });
-					},
-					dataUrl: "http://api.swiftype.com/api/v1/public/engines/suggest.json",
-					documentTypes: ['posts'],
-					engineKey: Swiftype.engineKey,
-					nameField: 'title',
-					attachTo: el,
-					disableAutocomplete: false,
-					filters: function() {
-						var catInput = $el.parents('form').find('#st-cat').first();
-						if (catInput) {
-							return {posts: {category: catInput.val()}};
-						}
-					}
-				});
+				$el.swiftype(swiftypeOptions);
 			});
 		});
 
