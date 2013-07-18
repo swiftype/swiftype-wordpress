d775f345ef325746b26f7839b1134a3a4b0e534c
Oleksiy Kovyrin <alexey@kovyrin.net>
2013-07-18 16:48:41 -0400

    Improve messaging for cases when phpunit is started w/o WP_TESTS_DIR environment variable

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,15 @@
 <?php
 
-require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+$wp_tests_dir = getenv('WP_TESTS_DIR');
+if (!$wp_tests_dir) {
+  die("Please define WP_TESTS_DIR environment variable and point it to your wordpress tests dir!\n");
+}
+
+require_once "$wp_tests_dir/includes/functions.php";
 
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../swiftype.php';
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+tests_add_filter('muplugins_loaded', '_manually_load_plugin');
 
-require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
-
+require "$wp_tests_dir/includes/bootstrap.php";
