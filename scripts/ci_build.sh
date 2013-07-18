1d51e3275b729edbfa2a6a3229076415284ee68e
Oleksiy Kovyrin <alexey@kovyrin.net>
2013-07-18 18:54:24 -0400

    Move test configs into tests directory, fold bin dir into the existing scripts directory

---
+++ scripts/ci_build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Fail on all errors
+set +ex
+
+export WP_TESTS_DIR=/tmp/wordpress-testing
+
+SCRIPTS_DIR=`dirname $0`
+WP_VERSION="$1"
+
+# Prepare testing frameworks and test database
+bash $SCRIPTS_DIR/install-wp-tests.sh wordpress_test root '' $WP_VERSION
+
+# Choose test configuration for single blog and MU wordpress
+if [ "$WP_MULTISITE" == "1" ]; then
+  TEST_CONFIG=tests/multisite.xml
+else
+  TEST_CONFIG=tests/phpunit.xml
+fi
+
+# Run tests
+exec phpunit -c $TEST_CONFIG
