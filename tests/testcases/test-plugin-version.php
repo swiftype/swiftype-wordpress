b72dbf65837f6ae8b6aad02f0fb37c6982472216
Oleksiy Kovyrin <alexey@kovyrin.net>
2013-07-19 11:49:43 -0400

    Fix comment

--- tests/testcases/test-plugin-version.php
+++ tests/testcases/test-plugin-version.php
@@ -1,7 +1,7 @@
 <?php
 
 class SwiftypePluginVersionTest extends SwiftypeTestCase {
-  // make sure the AKISMET_VERSION constant matches the version in the plugin info header
+  // make sure the SWIFTYPE_VERSION constant matches the version in the plugin info header
   function test_versions() {
     $plugin_data = get_plugin_data(SWIFTYPE_PLUGIN_DIR . '/swiftype.php');
     $this->assertEquals(SWIFTYPE_VERSION, $plugin_data['Version']);
