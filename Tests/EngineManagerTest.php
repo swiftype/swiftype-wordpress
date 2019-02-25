b2701f3940b90d8f62913b75f69d4c224c449308
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2019-02-25 14:50:51 +0100

    Test indexing hooks.

--- Tests/EngineManagerTest.php
+++ Tests/EngineManagerTest.php
@@ -9,6 +9,11 @@ use Swiftype\SiteSearch\Wordpress\Config\Config;
  */
 class EngineManagerTest extends AbstractTestCase
 {
+    private $indexingFilters = [
+        'future_to_publish', 'save_post', 'transition_post_status', 'trashed_post',
+        'swiftype_batch_post_index', 'swiftype_batch_post_delete',
+    ];
+
     /**
      * Test an engine is created when the engine is fully configured.
      *
@@ -28,5 +33,26 @@ class EngineManagerTest extends AbstractTestCase
 
         $this->assertEquals($this->getTestEngineName(), $engine['name']);
         $this->assertEquals($language, $engine['language']);
+    }
+
+    public function testIndexingHooksNotInstalled()
+    {
+        foreach($this->indexingFilters as $filter) {
+            $this->assertFalse(has_filter($filter));
+        }
+    }
+
+    public function testIndexingHooksInstalled()
+    {
+        foreach($this->indexingFilters as $filter) {
+            remove_all_actions($filter);
+        }
+
+        $this->loadDefaultConfig();
+
+        foreach($this->indexingFilters as $filter) {
+            $this->assertTrue(has_filter($filter));
+        }
+
     }
 }
